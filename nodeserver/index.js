6ffc832dacdfa6e028a354cdc71a86a78e4eb182
navin <navin@e-arth.in>
2022-03-24 14:56:37 +0530

    node server setup

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,7 +1,23 @@
 // import entire SDK
 var AWS = require('aws-sdk');
+// var AWS = require("aws-sdk");
+// Set the Region
+AWS.config.update({region: "us-west-2"});
 const express = require('express');
 const app = express();
+
+
+// Create DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: "2006-03-01"});
+
+// Call DynamoDB to retrieve the list of tables
+ddb.listTables({Limit:10}, function(err, data) {
+  if (err) {
+    console.log("Error", err.code);
+  } else {
+    console.log("Tables names are ", data.TableNames);
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('hello folks..')
