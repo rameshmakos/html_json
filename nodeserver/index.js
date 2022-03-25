0fb659a9430b82947f7dd0e6f5975a3233d711eb
navin <navin@e-arth.in>
2022-03-25 13:53:24 +0530

    qldb connection made

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,27 +1,104 @@
-// import entire SDK
-var AWS = require('aws-sdk');
-// var AWS = require("aws-sdk");
-// Set the Region
-AWS.config.update({region: "us-west-2"});
-const express = require('express');
+const express = require("express");
+// Load the Amazon QLDB Driver for Node.js
+var qldb = require("amazon-qldb-driver-nodejs");
+// Load the AWS SDK for node.js
+var AWS = require("aws-sdk");
+// Configure the SDK by setting the global configuration using 
+// AWS.Config
+var myConfig = new AWS.Config();
+myConfig.update({ region: "us-east-1" });
 const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-// Create DynamoDB service object
-var ddb = new AWS.DynamoDB({apiVersion: "2006-03-01"});
-
-// Call DynamoDB to retrieve the list of tables
-ddb.listTables({Limit:10}, function(err, data) {
-  if (err) {
-    console.log("Error", err.code);
-  } else {
-    console.log("Tables names are ", data.TableNames);
-  }
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+const createIndex = function (driver, callback) {
+  driver
+    .executeLambda(async (txn) => {
+      txn.execute("CREATE INDEX ON People (firstName)");
+    })
+    .then(callback);
+};
+// API to create table 'People' in the ledger 
+// named 'my-store-ledger' 
+app.get("/createTable", (req, res) => {
+  const driver = new qldb.QldbDriver("SPT-dev", myConfig);
+  driver
+    .executeLambda(async (txn) => {
+      txn.execute("CREATE TABLE People");
+    })
+    .then(() => {
+      createIndex(driver, function () {
+         driver.close();
+      });
+    })
+    .then((result) => {
+      res.send(JSON.stringify("{result: 'Created Table Successfully!'}"));
+    });
 });
-
-app.get('/', (req, res) => {
-  res.send('hello folks..')
-})
-
-
-app.listen(5000);
+// API to add a person in the table
+app.post("/addPerson", (req, res) => {
+  const driver = new qldb.QldbDriver("SPT-dev", myConfig);
+  const person = req.body;
+  driver
+    .executeLambda((txn) => {
+      txn.execute('INSERT INTO People `{"first_name":"John","last_name":"Doe", "age":"17"}`');
+      
+    })
+    .then(() => {
+      res.send(JSON.stringify("{result: 'Added Person Successfully!'}"));
+    })
+    .then((result) => {
+      driver.close();
+    });
+});
+// Get API to get the Person by First Name
+app.get("/getPerson", (req, res) => {
+  const driver = new qldb.QldbDriver("SPT-dev", myConfig);
+//   const firstName = req.params.firstName;
+//"SELECT * FROM People"
+//'SELECT (first_name || `@` || last_name) AS full_name FROM People'   --- FOR CONCAT QUERY PURPOSE
+  driver
+    .executeLambda(async (txn) => {
+      return txn.execute(
+        "SELECT * FROM People"  ,
+        
+      );
+    })
+    .then((result) => {
+      const resultList = result.getResultList();
+      //Pretty print the result list
+      console.log("The result List is ", JSON.stringify(resultList, null, 2));
+      res.send(JSON.stringify(resultList, null, 2));
+    })
+    .then((result) => {
+      driver.close();
+    });
+});
+// PUT API for updating a person last name based on first name
+app.get("/updatePersonLastName", (req, res) => {
+  const driver = new qldb.QldbDriver("SPT-dev", myConfig);
+//   const person = req.body;
+// txn.execute(
+//     "UPDATE People SET first_name = ? WHERE last_name = ?",
+//     person.lastName,
+//     person.firstName
+//   );
+    const person = { firstName: 'John', lastName: 'Gurram' };
+    var sql = "UPDATE People SET last_name = '"+person.lastName+"' WHERE first_name = 'John'"
+  driver
+    .executeLambda((txn) => {
+      txn.execute(
+        sql,
+      );
+    })
+    .then(() => {
+      res.send(JSON.stringify("{result: 'Person updated Successfully!'}"));
+    })
+    .then((result) => {
+      driver.close();
+    });
+});
---
+++ nodeserver/model/data.js
@@ -0,0 +1,33 @@
+export const PERSON = [
+    {
+        FirstName : "Raul",
+        LastName : "Lewis",
+        DOB : new Date("1963-08-19"),
+        Address : "1719 University Street, Seattle, WA, 98109",
+    },
+    {
+        FirstName : "Brent",
+        LastName : "Logan",
+        DOB : new Date("1967-07-03"),
+        Address : "43 Stockert Hollow Road, Everett, WA, 98203"
+
+    },
+    {
+        FirstName : "Alexis",
+        LastName : "Pena",
+        DOB : new Date("1974-02-10"),
+        Address : "4058 Melrose Street, Spokane Valley, WA, 99206"
+    },
+    {
+        FirstName : "Melvin",
+        LastName : "Parker",
+        DOB : new Date("1976-05-22"),
+        Address : "4362 Ryder Avenue, Seattle, WA, 98101",
+    },
+    {
+        FirstName : "Salvatore",
+        LastName : "Spencer",
+        DOB : new Date("1997-11-15"),
+        Address : "4450 Honeysuckle Lane, Seattle, WA, 98101",
+    }
+];
